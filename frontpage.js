becbddceff5b04c927dfa197c52a2d5246971f46
Ishwarya Madupu <117265891+IshwaryaMadupu@users.noreply.github.com>
2024-06-08 10:37:35 +0530

    Add files via upload

---
+++ frontpage.js
@@ -0,0 +1,19 @@
+﻿document.getElementById('class-6').addEventListener('click', () => {
+    window.location.href = 'class6.html';
+});
+
+document.getElementById('class-7').addEventListener('click', () => {
+    window.location.href = 'class7.html';
+});
+
+document.getElementById('class-8').addEventListener('click', () => {
+    window.location.href = 'class8.html';
+});
+
+document.getElementById('class-9').addEventListener('click', () => {
+    window.location.href = 'class9.html';
+});
+
+document.getElementById('class-10').addEventListener('click', () => {
+    window.location.href = 'class10.html';
+});
